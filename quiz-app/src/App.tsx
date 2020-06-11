2d889cd2b78f1f838ce42cf9ebd11b9da82de6b8
CalciumG <callumgo@outlook.com>
2020-06-11 01:13:04 +0100

    Added button

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import "./App.css";
 import { Jumbotron, Button } from "react-bootstrap";
-import Quiz from "./Components/Quiz";
+import Quiz from "./Components/quiz";
 
 function App() {
+  const Search = () => {
+    const [showResults, setShowResults] = React.useState(false);
+    const onClick = () => setShowResults(true);
+    return (
+      <div>
+        <input type="submit" value="Search" onClick={onClick} />
+        {showResults ? <Results /> : null}
+      </div>
+    );
+  };
+
+  const Results = () => (
+    <div id="results" className="search-results">
+      Some Results
+    </div>
+  );
+
   return (
     <div className="App">
       <div>
@@ -11,7 +28,7 @@ function App() {
           <h1>Cal's Quiz</h1>
           <p>Welcome to my quiz! Click the Button below to get started.</p>
           <p>
-            <Button variant="primary">Start</Button>
+            <Search />
           </p>
         </Jumbotron>
       </div>
