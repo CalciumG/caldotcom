5be7f8f8a0d2e1da07e820417d3e3fb6d10da374
Callum <callumgo@outlook.com>
2020-10-31 18:49:25 +0000

    remove stuff

--- calciumdevelopment/calcium_development/src/components/HelloWorld.vue
+++ calciumdevelopment/calcium_development/src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <div class="bgShape" min-height="70vh" rounded="lg"></div>
-      <div class="headerTextWrapper">
-        <h1>TEST</h1>
+      <div class="bgShape show-on-scroll" min-height="70vh" rounded="lg"></div>
+      <div class="headerTextWrapper ">
+        <h1 class="headerText">TEST</h1>
         <br />
-        <h1>TEXT</h1>
+        <h1 class="headerText">TEXT</h1>
         <br />
       </div>
     </v-main>
@@ -32,7 +32,7 @@ export default Vue.extend({
   height: 110vh;
   transform: translate3d(0px, 0px, 0px) scale3d(0.9, 0.9, 1) rotateX(0deg)
     rotateY(0deg) rotateZ(-20deg) skew(0deg, 0deg);
-  background-color: rgb(2, 237, 237);
+  background-color: rgb(50, 57, 237);
   z-index: 0;
 }
 
@@ -49,5 +49,33 @@ export default Vue.extend({
   z-index: 1;
   padding: 0;
   margin: 0;
+}
+
+.colour-change-bg {
+  position: absolute;
+  left: 0%;
+  top: 0%;
+  right: 0%;
+  bottom: auto;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  -webkit-flex-wrap: wrap;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  background-color: #e2e6e1;
+  -webkit-transform-origin: 50% 50%;
+  -ms-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+  -o-object-fit: fill;
+  object-fit: fill;
+}
+
+.headerText {
+  font-size: 10em;
+  color: white;
 }
 </style>
