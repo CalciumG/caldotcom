cfdf7bf03315ea2698bde90b09827b4c07b88357
Callum <callumgo@outlook.com>
2021-08-03 22:43:43 +0100

    Init commit

---
+++ cal-dot-com/src/App.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import { Button } from "antd";
+import "./App.css";
+
+import { Layout, Menu } from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const App = () => (
+  <div className="App">
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+          <Menu.Item key="4" icon={<UserOutlined />}>
+            nav 4
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header
+          className="site-layout-sub-header-background"
+          style={{ padding: 0 }}
+        />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: "100%" }}
+          >
+            content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  </div>
+);
+
+export default App;
--- f1-results/src/App.js
+++ f1-results/src/App.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useState, Fragment } from "react";
+import React, { useEffect, useState } from "react";
 import Tabletop from "tabletop";
+import { Card } from "antd";
+import "antd/dist/antd.css";
+
 import "./index.css";
 
 export default function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
+  useEffect((data) => {
     Tabletop.init({
-      key: "1o3CgOq_vdSmh3tkkVG-S4y-X79tZlu0esJZ0WBGwzu8",
+      key: "https://docs.google.com/spreadsheets/d/1o3CgOq_vdSmh3tkkVG-S4y-X79tZlu0esJZ0WBGwzu8/edit?usp=sharing",
       simpleSheet: false,
     })
       .then((data) => setData(data.PointsTable))
       .catch((err) => console.warn("ERROR", err));
   }, []);
 
-  console.log(data);
-
   return (
     <ul>
       {data.elements !== undefined &&
         data.elements.map((items) => (
-          <li key={items.Driver}>{items.Driver}</li>
+          <Card
+            key={items.Driver}
+            title="Default size card"
+            style={{ width: 300 }}
+          >
+            <p>{items.Driver}</p>
+            <p>{items.ChampionshipPoints}</p>
+            <p>Card content</p>
+          </Card>
         ))}
     </ul>
   );
