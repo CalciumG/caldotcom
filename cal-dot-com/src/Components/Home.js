38500ad2d735b83fbba98f8d0814205a761065da
Callum <callumgo@outlook.com>
2021-08-06 01:22:55 +0100

    Colour update

--- cal-dot-com/src/Components/Home.js
+++ cal-dot-com/src/Components/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-teal-600 rounded text-lg"
             >
               Work With Me
             </a>
